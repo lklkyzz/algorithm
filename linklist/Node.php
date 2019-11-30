bbb5049e79644676be5eb777a5f87e52cd0c6220
随机的鲤鱼王 <lklkyzz@github.com>
2019-11-30 23:47:53 +0800

    单链表反转

--- linklist/Node.php
+++ linklist/Node.php
@@ -6,9 +6,9 @@
 class Node {
     public $value;//节点值
     public $next;//下一个节点
-    public function __construct($value=null, $next=null) {
+    public $pre;//上一个节点
+    public function __construct($value=null) {
         $this->value = $value;
-        $this->next = $next;
     }
 }
 
--- linklist/SingleLinkList.php
+++ linklist/SingleLinkList.php
@@ -1,13 +1,15 @@
 <?php
 /*
-* 链表基础操作
-*
-*/
+ * 链表基础操作
+ *
+ */
 require 'Node.php';
-class LinkList {
-    private $header;//头节点
+class LinkList
+{
+    private $header; //头节点
 
-    public function __construct($value = null, $next = null) {
+    public function __construct($value = null, $next = null)
+    {
         $this->header = new Node($value, $next);
     }
 
@@ -16,13 +18,14 @@ class LinkList {
      * @param $point 插入位置
      * @param $node 插入节点
      */
-    public function addLink($node, $point=null) {
+    public function addLink($node, $point = null)
+    {
         $current = $this->header;
         $i = 1;
-        while ( $current->next != null ) {
-            if ( $point != null && $point != 0 && $point <= $i ) {
+        while ($current->next != null) {
+            if ($point != null && $point != 0 && $point <= $i) {
                 //按$point插入
-                if ( $point == $i ) {
+                if ($point == $i) {
                     break;
                 }
             }
@@ -37,22 +40,23 @@ class LinkList {
     /**
      * 删除指定节点
      */
-    public function delLink($point) {
-        if ( $point == null || $point == 0 ) {
+    public function delLink($point)
+    {
+        if ($point == null || $point == 0) {
             echo "非法节点<br>";exit;
         }
         $i = 1;
         $current = $this->header;
-        while ( $current->next != null ) {
-            if ( $point == $i ) {
+        while ($current->next != null) {
+            if ($point == $i) {
                 break;
             }
             $current = $current->next;
             $i++;
         }
 
-        //删除节点前节点
-        if ( $point < $i ) {
+        //当前节点为删除节点的前节点
+        if ($point < $i) {
             $current->next = $current->next->next;
         } else {
             echo "非法节点<br>";exit;
@@ -62,10 +66,11 @@ class LinkList {
     /**
      * 遍历节点
      */
-    public function getLinkList($node) {
+    public function getLinkList()
+    {
         $current = $this->header;
         $i = 1;
-        while ( $current->next != null ) {
+        while ($current->next != null) {
             echo "当前第{$i}节点值为：{$current->next->value}<br>";
             $current = $current->next;
             $i++;
@@ -76,24 +81,41 @@ class LinkList {
      * 获取链表长度
      * @param $node 链表
      */
-    public function getLinkLength($node) {
+    public function getLinkLength()
+    {
         $i = 0;
         $current = $this->header;
-        while ( $current->next != null ) {
+        while ($current->next != null) {
             $i++;
             $current = $current->next;
         }
         return $i;
     }
 
+    /**
+     * 反转链表
+     */
+    public function reverseList($link) {
+        $current = $link->header->next;
+        if ($current == null) {
+            return null;
+        }
+        $pre = null;
+
+        while ($current != null) {
+            $tmp = $current->next;
+            $current->next = $pre;
+            $pre = $current;
+            $current = $tmp;
+        }
+    }
 
 }
 
 $link = new LinkList();
-$link->addLink(new Node('html'));
-$link->addLink(new Node('php'), 1);
-$link->addLink(new Node('js'), 8);
-$link->addLink(new Node('css'), 3);
-$link->getLinkList($link);
-$link->delLink(0);
-// $link->getLinkList($link);
+$link->addLink(new Node(1));
+$link->addLink(new Node(2));
+$link->addLink(new Node(3));
+// $link->addLink(new Node('css'), 3);
+// $link->getLinkList();
+$link->ReverseList($link);
